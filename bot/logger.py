020ec21e88147dac8c7c8909a229bb70ae27bb88
Лев <90714959+BR7RKR@users.noreply.github.com>
2023-10-25 22:15:37 +0300

    убрал комментарии

--- bot/logger.py
+++ bot/logger.py
@@ -5,18 +5,14 @@ def setup_logger():
     log = logg.getLogger(__name__)
     log.setLevel(logg.DEBUG)
 
-    # Создание обработчика для уровня INFO
     info_handler = logg.FileHandler('_logs/info.log')
     info_handler.setLevel(logg.INFO)
 
-    # Создание обработчика для ошибок с уровнями выше WARNING
     error_handler = logg.FileHandler('_logs/errors.log')
     error_handler.setLevel(logg.ERROR)
 
-    # Создание форматтера для логов
     formatter = logg.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
-    # Привязка обработчика и форматтера к логгеру
     info_handler.setFormatter(formatter)
     error_handler.setFormatter(formatter)
 
